0abc4bad8ca0360a119ff3e044cebabeae747a7c
Antony Kithinzi <kithinzi97@gmail.com>
2022-05-31 11:24:00 -0700

    edited task file 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if c != 102 and str(chr(c)) == 'q':
-       print(end="{}".format(chr(c)))
+    if c != 101 and str(chr(c)) != 'q':
+        print(end="{}".format(chr(c)))
