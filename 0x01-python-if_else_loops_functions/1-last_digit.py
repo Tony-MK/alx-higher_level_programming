1b8f4c3d69c45d97b101be7ffccb30963e9fa3fc
Antony Kithinzi <kithinzi97@gmail.com>
2022-05-31 11:02:54 -0700

    added first task fifle

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10 if number >= 0 else -(abs(number) % 10)
+if last_digit == 0:
+    str = 'zero'
+else:
+    str = ('greater than 5' if number > 5 else 'less than 6')
+print(f'Last digit of {number} is {last_digit} and is {str}')
