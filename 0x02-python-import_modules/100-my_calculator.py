b86822c7dbfa6fc175ae03eebf3f3a4e152f0c10
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-02 16:40:45 -0700

    edited task files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-cal = __import__("calculator_1")
-
-OPERATORS = {'+': cal.add, '-': cal.sub, '*': cal.mul, '/': cal.div}
-ERROR_1 = "Unknown operator. Available operators: +, -, * and /"
-
 if __name__ == "__main__":
+    import sys
+
+    cal = __import__("calculator_1")
+
+    OPERATORS = {'+': cal.add, '-': cal.sub, '*': cal.mul, '/': cal.div}
+    ERROR_1 = "Unknown operator. Available operators: +, -, * and /"
+
     if len(sys.argv) != 4:
         sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
