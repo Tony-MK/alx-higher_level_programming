69ddd8ea6c54689490ea21082fb6201e5fccbcae
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-02 06:23:26 -0700

    added py file for the third task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    n_args = len(sys.argv) - 1
+    if n_args == 0:
+        print(n_args, "arguments.")
+
+    else:
+        print(n_args, "arguments:\n" + "\n".join([
+            "{}: {}".format(index + 1, arg)
+            for index, arg in enumerate(sys.argv[1:])
+        ]))
