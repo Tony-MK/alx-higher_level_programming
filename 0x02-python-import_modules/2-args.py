5288e71d71cecf3616cde73d237b4fb2a6fdbc9d
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-02 16:46:04 -0700

    edited 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
         print(n_args, "arguments.")
 
     else:
-        print(n_args, "arguments:\n" + "\n".join([
+        print("{} argument{}:".format(n_args, "s" if n_args > 1 else ""))
+        print("\n".join([
             "{}: {}".format(index + 1, arg)
             for index, arg in enumerate(sys.argv[1:])
         ]))
