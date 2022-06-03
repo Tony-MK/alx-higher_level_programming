4717bed20d1d59f616e008914b11d0553bdaaa90
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-02 18:26:15 -0700

    refactored 100-my_calculator.py file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,16 +3,16 @@ if __name__ == "__main__":
     import sys
     import calculator_1 as cal
 
-    OPERATORS = {'+': cal.add, '-': cal.sub, '*': cal.mul, '/': cal.div}
+    OPS = {'+': cal.add, '-': cal.sub, '*': cal.mul, '/': cal.div}
     ERROR_1 = "Unknown operator. Available operators: +, -, * and /\n"
 
     if len(sys.argv) != 4:
-        sys.stderr.write("Usage: " + argv[0] + " <a> <operator> <b>\n")
+        print(end="Usage: " + sys.argv[0] + " <a> <operator> <b>\n")
         sys.exit(1)
     elif sys.argv[2] not in OPERATORS:
-        sys.stderr.write(ERROR_1)
+        print(end=ERROR_1)
         sys.exit(1)
     a: int = int(sys.argv[1])
     b: int = int(sys.argv[3])
-    print("{:d} {} {:d}".format(a, sys.argv[2], b), end=" = ")
-    print(OPERATORS[sys.argv[2]](a, b))
+    op: str = sys.argv[2]
+    print("{:d} {} {:d} = {}".format(a, op, b, OPS[op](a, b)))
