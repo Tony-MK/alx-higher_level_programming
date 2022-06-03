4e264a11f7d3819a25eee433150fe6a0733414c5
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-02 17:12:46 -0700

    edited 103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-list(map(lambda c: print(end=chr(c)), list(range(65, 91)) + [10]))
+if __name__ == "__main__":
+    list(map(lambda c: print(end=chr(c)), list(range(65, 91)) + [10]))
