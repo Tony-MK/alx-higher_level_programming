515601e3705c88b3e6554299a54fef30e2010893
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-04 15:16:41 -0700

    edited second task file

--- 0x03-python-data_structures/2-main.py
+++ 0x03-python-data_structures/2-main.py
@@ -1,4 +1,5 @@
-ce_in_list = __import__('2-replace_in_list').replace_in_list
+#!/usr/bin/python3
+replace_in_list  = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
