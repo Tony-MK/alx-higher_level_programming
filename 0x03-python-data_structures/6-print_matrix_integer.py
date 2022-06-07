32e24e013b8a4e8419fa30d8e8b9a9498ba571f5
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-06 18:22:39 -0700

    fixed code for the sixth task file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    list(map(lambda row: print(" ".join(
-        list(map(lambda num: "{:d}".format(num), row)))),
-            matrix
-    ))
+    if matrix is None:
+        return
+    for row in matrix:
+        print(" ".join(
+            list(map(lambda num: "{:d}".format(num), row))))
