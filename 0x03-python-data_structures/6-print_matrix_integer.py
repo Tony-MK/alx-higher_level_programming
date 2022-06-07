b9e13a01278944a9cad88fccb6b386bdeb68b72d
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-06 17:49:07 -0700

    added tsk 6 file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    print("\n".join(
+        list(map(lambda row: " ".join(
+            list(map(lambda num: "{}".format(num), row))),
+            matrix
+        )))
+    )
