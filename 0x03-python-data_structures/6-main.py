96c842b94c9edf6e78d545cf3809b6899af894f6
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-06 18:29:22 -0700

    fixed code for the sixth task file

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -4,7 +4,11 @@ print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 matrix = [
             [1, 2, 3],
             [4, 5, 6],
-            [7, 8, 9]
+            [7, 8, 9],
+            [],
+            [10],
+            [11, 12],
+            [13, 14, 15],
         ]
 
 print_matrix_integer(matrix)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
     for row in matrix:
+        if len(row) == 0:
+            print()
+            continue
+
         for index in range(len(row)-1):
             print(end="{:d} ".format(row[index]))
-        print(":d".format(row[index]))
+        print("{:d}".format(row[len(row) - 1]))
