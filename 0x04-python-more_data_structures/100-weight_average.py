343ba5696a8c8b337bcdb1d3ec39e93b0f4382ea
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-10 18:20:09 -0700

    correcting 100 task function file

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list):
+    if len(my_list) == 0:
+        return 0
     weights_sum: int = sum(map(lambda x: x[1], my_list))
     return sum(map(lambda x: x[0] * x[1], my_list)) / weights_sum
