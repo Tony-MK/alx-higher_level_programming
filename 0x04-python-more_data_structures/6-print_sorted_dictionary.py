bd3e264db89445c758dbf55ed32f499a1edd94e4
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-10 19:24:46 -0700

    corrected project task files 6 and 103

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if len(a_dictionary) == 0:
+        return
     sorted_keys = list(a_dictionary.keys())
     sorted_keys.sort(reverse=False)
     print(
