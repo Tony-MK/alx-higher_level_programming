b0b1a669ebbcfb6b43c25780a23fbcc72dc84536
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-20 07:51:13 -0700

    Added task file 2 python file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print(end="{:d}".format(my_list[i]))
+            n += 1
+        except (TypeError, ValueError) as e:
+            pass
+    print(end="\n")
+    return n
