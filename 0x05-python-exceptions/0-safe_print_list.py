09af4bba69ceb58b050c9177b29ca6c844a35576
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-20 08:10:52 -0700

    edited python files linux headers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     n = 0
     for index in range(x):
         try:
             print(end="{}".format(my_list[index]))
             n += 1
-        except IndexError as e:
+        except Exception as e:
             break
     print()
     return n
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/ptyhon3
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
     for i in range(x):
