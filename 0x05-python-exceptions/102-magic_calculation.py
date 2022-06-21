f280928c53763988dfa311de4e009da2e95c2526
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-20 19:24:46 -0700

    edited 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    result = 1
-    for i in range(77, 102):
-        if i > a:
-            raise Exception("Too far")
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+                break
+        except:
+            result = b + a
             break
-        b = 1
-        a = 0
-        result += (a ** b) / i
-
-    result = (a + b)
     return result
