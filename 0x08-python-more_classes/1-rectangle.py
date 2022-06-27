d03fe6324d607d4fa93c3eb79f418d6220b26994
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-27 14:59:03 -0700

    Removing 2-rectangle evxtra functions from task 1 file and edited hieght value error string and considered a float

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,8 +19,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int): raise TypeError("width must be an integer")
-        elif value < 0: raise ValueError("width must be >= 0")
+        if not (isinstance(value, int) or isinstance(value, float)):
+            raise TypeError("width must be an integer")
+        elif value < 0: 
+            raise ValueError("width must be >= 0")
         self.__width = value
     
     @property
@@ -29,18 +31,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int): raise TypeError("height must be and integer")
-        elif value < 0: raise ValueError("height must be >= 0")
+        if not (isinstance(value, int) or isinstance(value, float)): 
+            raise TypeError("height must be an integer")
+        elif value < 0: 
+            raise ValueError("height must be >= 0")
         self.__height = value
-
-    def area(self):
-        """
-        Return the area of the rectangle
-        """
-        return self.width * self.height
-
-    def perimeter(self):
-        """
-        Return the perimeter of the rectangle
-        """
-        return 2 * (self.width + self.height)
