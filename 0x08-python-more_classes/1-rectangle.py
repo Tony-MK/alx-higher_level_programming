990deb29e52bd077ac932ea2af427f22959d895e
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-27 17:50:38 -0700

    Adding setter and getter function documenation for width an height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,26 +12,30 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
-    
+
     @property
     def width(self):
+        """Getter for the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) == int:
+        """Setter for the width"""
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0: 
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
+        """Getter for the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) == int: 
+        """Setter for the height"""
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0: 
             raise ValueError("height must be >= 0")
