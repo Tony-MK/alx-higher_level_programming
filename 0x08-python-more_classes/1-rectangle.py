e6940269191a4d4b00ab1ca783ed0e0e28543395
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-27 17:02:21 -0700

    Revert "casting w and h parameters if float to and integer"
    This reverts commit e0ceecd56c49b567b4ec25b45fb1934f07c8d336.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0: 
             raise ValueError("width must be >= 0")
-        self.__width = int(value)
+        self.__width = value
     
     @property
     def height(self):
@@ -35,4 +35,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0: 
             raise ValueError("height must be >= 0")
-        self.__height = int(value)
+        self.__height = value
