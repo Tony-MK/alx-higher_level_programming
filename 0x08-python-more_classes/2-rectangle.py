eebf7eae0f64280d3ab17dc636b8c74d21e4a902
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-27 18:13:21 -0700

    Added task two py files

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle" module
+"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Object constructor"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Getter for the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+    
+    def area(self):
+        """Return area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
