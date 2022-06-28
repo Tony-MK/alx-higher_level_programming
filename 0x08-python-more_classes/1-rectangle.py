8e2c776bbc2969e4ffdb97400456f49b982ca1e7
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-27 17:58:25 -0700

    Pydocstyle editing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        """Object constructor"""
         self.__height = height
         self.__width = width
 
@@ -23,9 +24,10 @@ class Rectangle:
         """Setter for the width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0: 
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -37,6 +39,7 @@ class Rectangle:
         """Setter for the height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0: 
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
