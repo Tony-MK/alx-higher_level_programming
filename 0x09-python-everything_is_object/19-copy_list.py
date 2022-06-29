c741ae8dfad24083eddc03fba06877bb794fbea4
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-28 18:09:04 -0700

    Adding and correcting task 19 python files

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def copy_list(l):
-    return type(l)((copy_list(e) if type(e) in [dict, list, set] else e for e in l))
+    return type(l)((copy_list(e) if hasattr(e, "__iter__") else e for e in l))
