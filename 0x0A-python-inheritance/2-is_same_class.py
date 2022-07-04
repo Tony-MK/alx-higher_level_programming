abf896a751fcdf2651b98c0aa6220301b435cf58
Antony Kithinzi <kithinzi97@gmail.com>
2022-07-04 16:00:02 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+"is_same_class" module
+"""
+
+
+def is_same_class(obj, a_class):
+    """Return if the object is an instance of class"""
+    return isinstance(obj, a_class)
