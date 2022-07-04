3a4f0fe5e0729c1163cca3167cecc4d8ac8e2822
Antony Kithinzi <kithinzi97@gmail.com>
2022-07-04 16:30:41 -0700

    Correcting 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,6 +6,4 @@
 
 def is_kind_of_class(obj, a_class):
     """Return if the object is an instance of class or derived"""
-    if type(obj) == a_class or issubclass(obj, a_class):
-        return True
     return isinstance(obj, a_class)
