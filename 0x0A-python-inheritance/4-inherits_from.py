b47f33cf5258784696dea2ca8e75c28d98eda65f
Antony Kithinzi <kithinzi97@gmail.com>
2022-07-04 16:32:54 -0700

    Correcting 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@
 
 def inherits_from(obj, a_class):
     """Return if the object is inherited from the class"""
-    return issubclass(obj, a_class)
+    return isinstance(obj, a_class) or issubclass(obj, a_class)
