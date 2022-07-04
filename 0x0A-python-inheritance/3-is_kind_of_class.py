24d74ba70f3903dc6d7ecd115bf2fd652504c946
Antony Kithinzi <kithinzi97@gmail.com>
2022-07-04 16:24:12 -0700

    3 & 4 files

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+"is_kind_of_class" module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return if the object is an instance of class or derived"""
+    return isinstance(obj, a_class) or issubclass(obj, a_class)
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+"inherits_from" module
+"""
+
+
+def inherits_from(obj, a_class):
+    """Return if the object is inherited from the class"""
+    return issubclass(obj, a_class)
