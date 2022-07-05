7c0d51c30a2cdd1bad16cb8e5e9b3e81cb96a9aa
Antony Kithinzi <kithinzi97@gmail.com>
2022-07-05 04:48:24 -0700

    utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 """
 
 def read_file(filename=""):
-    with open(filename, "r") as f:
-        print(f.read())
+    with open(filename, "r", "UTF-8") as f:
+        for l in f:
+            print(l)
