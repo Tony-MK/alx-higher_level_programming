98bad8f364c2f2571869c13369d2babcebe6d8cd
Antony Kithinzi <kithinzi97@gmail.com>
2022-07-05 06:15:35 -0700

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+"from_json_string" function module
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object from a JSON string"""
+    return json.loads(my_str)
